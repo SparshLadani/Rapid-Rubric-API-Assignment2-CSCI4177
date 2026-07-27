a0350ad76c6115a3eb494ea3b8fff75d234e7d60
Sparsh Ladani <sp835101@dal.ca>
2026-07-27 17:36:21 -0300

    Added JMeter results, ZAP report, and monitoring config files!

--- index.js
+++ index.js
@@ -4,11 +4,20 @@ import rateLimit from "express-rate-limit";
 import submissionsRouter from "./routes/submissions.js";
 import compression from 'compression';
 import helmet from 'helmet';
+import { register, httpRequestDuration } from './middleware/metrics.js';
 
 const app = express();
 app.use(helmet());
 app.use(express.json({ limit: '50mb' }));
 app.use(compression());
+app.use((req, res, next) => {
+  const end = httpRequestDuration.startTimer();
+  res.on('finish', () => {
+    end({ method: req.method, route: req.path, status: res.statusCode });
+  });
+  next();
+});
+
 
 app.use(rateLimit({
     windowMs: 60*1000,
@@ -17,6 +26,11 @@ app.use(rateLimit({
 }));
 
 app.use('/api/v1', submissionsRouter);
+
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', register.contentType);
+  res.end(await register.metrics());
+});
 
 app.use((req, res) => {
     res.status(404).json({ error: { code: 'NOT_FOUND', message: 'Route not found.' } });
---
+++ middleware/metrics.js
@@ -0,0 +1,14 @@
+import client from 'prom-client';
+
+const register = new client.Registry();
+client.collectDefaultMetrics({ register });
+
+const httpRequestDuration = new client.Histogram({
+  name: 'http_request_duration_ms',
+  help: 'Duration of HTTP requests in ms',
+  labelNames: ['method', 'route', 'status'],
+  buckets: [50, 100, 200, 500, 1000, 2000],
+  registers: [register]
+});
+
+export { register, httpRequestDuration };
